83eb4ba2501a1e79c35611ee7406e7a82da6e898
Johnson Li <johnson.li61001@gmail.com>
2021-10-06 19:54:59 -0400

    update .gitignore

--- Assets/Scripts/IMovable.cs
+++
@@ -1,4 +0,0 @@
-public interface IMovable
-{
-    public MovementManager Movement { get; }
-}
--- Assets/Scripts/Weapons/Rarity.cs
+++
@@ -1,7 +0,0 @@
-public enum Rarity
-{
-    Common = 1,
-    Rare = 2,
-    Epic = 3,
-    Legend = 4
-}
