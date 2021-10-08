b7d5a1674baf0f8a1dd3d6c91d7e7abc9cd80a03
smlim991 <47668659+smlim991@users.noreply.github.com>
2021-10-07 21:49:44 -0400

    Enemy dies when hit with bullet

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -18,4 +18,10 @@ public class Enemy : MonoBehaviour
     {
         transform.position = Vector2.MoveTowards(transform.position,target.position,speed*Time.deltaTime);
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Bullet"){
+            Destroy(gameObject); 
+        }
+    }
 }
