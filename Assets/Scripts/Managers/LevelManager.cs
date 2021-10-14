6a901ce87edc325f826365f269d5ca437da5531b
PIG208 <359101898@qq.com>
2021-10-13 21:40:18 -0400

    Add LevelManager

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>Each level needs to have a LevelManager, and only one LevelManager can exist in each level</summary>
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager CurrentLevelManager { get; private set; }
+    public Player Player;
+
+    private void Start()
+    {
+        CurrentLevelManager = this;
+    }
+}
