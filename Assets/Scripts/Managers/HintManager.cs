d2bfecaf9d2251c048cb1ab8f091c380cf3de54a
smlim991 <47668659+smlim991@users.noreply.github.com>
2021-10-14 21:39:14 -0400

    hint added

---
+++ Assets/Scripts/Managers/HintManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintManager : MonoBehaviour
+{
+    public Text Hint;
+
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        IEnumerator Wait(){
+            
+            yield return new WaitForSeconds(2f);
+            
+            Hint.text = "";
+        
+        }
+        if(other.gameObject.tag=="Player"){
+            Hint.text = "Press q or lft shft";
+            StartCoroutine(Wait());
+        }
+    }
+}
