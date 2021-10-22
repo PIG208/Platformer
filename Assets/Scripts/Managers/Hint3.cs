0b5baa20ea5818e1a4cef6db478a9ce4555a7a5d
PIG208 <359101898@qq.com>
2021-10-21 21:22:27 -0400

    Fix hint

--- Assets/Scripts/Managers/Hint3.cs
+++ Assets/Scripts/Managers/Hint3.cs
@@ -1,20 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hint3 : MonoBehaviour
 {
-     public Text Hint;
+    public Text Hint;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        IEnumerator Wait(){
-            
+        IEnumerator Wait()
+        {
+
             yield return new WaitForSeconds(2f);
-            
+
             Hint.text = "";
-        
+
         }
-        if(other.gameObject.tag=="Player"){
+        if (other.gameObject.tag == "Player")
+        {
             Hint.text = "Use Scroll Wheel";
             StartCoroutine(Wait());
         }
